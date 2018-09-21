6c51ac03e5a6e684e01f6e72c915d8236d46b68c
Francisco Javier Luna Vázquez <frahik@gmail.com>
2018-09-21 14:37:23 -0500

    New methods implemented
    Now summary.IBCF() has more options for more detailed summary.
    Now summary.IBCFY() has more options for more detailed summary.
    Now the summaries export the MAAPE index (before was MSEP)
    Added print methods for the objects IBCF and IBCFY

--- R/methods.R
+++ R/methods.R
@@ -3,35 +3,78 @@
 #' @description Summary of IBCF object
 #'
 #' @param object \code{IBCF object} IBCF object, result of use the IBCF() function
+#' @param information \code{string} ...
+#' @param digits \code{numeric} ...
 #' @param ... Further arguments passed to or from other methods.
 #'
+#' @importFrom stats cor
+#' @importFrom dplyr summarise group_by select '%>%' mutate_if funs
+#'
 #' @export
-summary.IBCF <- function(object,...){
+summary.IBCF <- function(object, information = 'compact', digits = 4, ...){
   if (!inherits(object, "IBCF")) stop("This function only works for objects of class 'IBCF'")
-  return(object$predictions_Summary)
-}
 
+  object$predictions_Summary %>%
+    group_by(Environment, Trait, Partition) %>%
+    summarise(Pearson = cor(Predicted, Observed, use = 'pairwise.complete.obs'),
+              MAAPE = mean(atan(abs(Observed-Predicted)/abs(Observed)))) %>%
+    select(Environment, Trait, Partition, Pearson, MAAPE) %>%
+    mutate_if(is.numeric, funs(round(., digits))) %>%
+    as.data.frame() -> presum
+
+  presum %>%  group_by(Environment, Trait) %>%
+    summarise(SE_MAAPE = sd(MAAPE, na.rm = T)/sqrt(n()), MAAPE = mean(MAAPE, na.rm = T),
+              SE_Pearson = sd(Pearson, na.rm = T)/sqrt(n()), Pearson = mean(Pearson, na.rm = T))  %>%
+    select(Environment, Trait, Pearson, SE_Pearson, MAAPE, SE_MAAPE) %>%
+    mutate_if(is.numeric, funs(round(., digits))) %>%
+    as.data.frame() -> finalSum
+
+  out <- switch(information,
+                compact = finalSum,
+                complete = presum,
+                extended = {
+                  finalSum$Partition <- 'All'
+                  presum$Partition <- as.character(presum$Partition)
+                  presum$SE_Pearson <- NA
+                  presum$SE_MAAPE <- NA
+                  rbind(presum, finalSum)
+                }
+  )
+  return(out)
+}
 
 #' @title Summary
 #'
 #' @description Summary of IBCFY object
 #'
 #' @param object \code{IBCFY object} IBCFY object, result of use the IBCF.Years() function
+#' @param digits \code{numeric} Number of digits of the output.
 #' @param ... Further arguments passed to or from other methods.
 #'
+#' @importFrom stats cor
+#' @importFrom dplyr summarise group_by select '%>%' mutate_if funs
+#'
 #' @export
-summary.IBCFY <- function(object,...){
+summary.IBCFY <- function(object, digits = 4, ...) {
   if (!inherits(object, "IBCFY")) stop("This function only works for objects of class 'IBCFY'")
-  return(object$predictions_Summary)
-}
 
+  object$predictions_Summary %>%
+    group_by(Environment, Trait) %>%
+    summarise(Pearson = cor(Predicted, Observed, use = 'pairwise.complete.obs'),
+              MAAPE = mean(atan(abs(Observed-Predicted)/abs(Observed)))) %>%
+    select(Environment, Trait, Pearson, MAAPE) %>%
+    mutate_if(is.numeric, funs(round(., digits))) %>%
+    as.data.frame() -> out
+
+  return(out)
+}
 
 #' @title Plot IBCF graph
 #'
 #' @description Plot from IBCF object
 #'
 #' @param x \code{IBCF object} IBCF object, result of use the IBCF() function
-#' @param select \code{character} By default ('Pearson'), plot the Pearson Correlations of the IBCF Object, else ('MSEP'), plot the MSEP of the IBCF Object.
+#' @param select \code{character} By default ('Pearson'), plot the Pearson Correlations of the IBCF Object, else ('MAAPE'), plot the MAAPE of the IBCF Object.
 #' @param ... Further arguments passed to or from other methods.
 #'
 #' @importFrom graphics arrows axis plot
@@ -40,24 +83,24 @@ plot.IBCF <- function(x, select = 'Pearson', ...){
   ### Check that object is compatible
   if (!inherits(x, "IBCF")) stop("This function only works for objects of class 'IBCF'")
 
-  results <- x$predictions_Summary
-
-  results$Trait_Env <- results$Trait_Env[order(results[, select])]
+  results <- summary(x)
   results[, select] <- results[order(results[, select]), select]
 
   if (select == "Pearson") {
-    results$SE <- results$SE_Cor * 1.96
+    results$SE <- results$SE_Pearson * 1.96
     ylab <- "Pearson's Correlation"
-  } else if (select == "MSEP") {
-    results$SE <- results$SE_MSEP * 1.96
+  } else if (select == "MAAPE") {
+    results$SE <- results$SE_MAAPE * 1.96
     ylab <- select
   }
 
-  x <- 1:length(results$Trait_Env)
-  plot(x, results[, select], ylim = range(c(results[, select] - results$SE, results[, select] + results$SE)),
-       type = 'p', ylab = ylab, xlab = '', xaxt = "n", las = 2, ...)
-  axis(1, at = x, labels = results$Trait_Env, las = 2)
-  arrows(x, results[, select] - results$SE, x, results[, select] + results$SE, code = 3, length = 0.02, angle = 90)
+  x.labels <- paste0(results$Trait, '_', results$Env)
+  plot.x <- 1:length(x.labels)
+
+  plot(plot.x, results[, select], ylim = range(c(results[, select] - results$SE, results[, select] + results$SE)),
+      type = 'p', ylab = ylab, xlab = '', xaxt = "n", ...)
+  axis(1, at = plot.x, labels = x.labels, las = 2)
+  arrows(plot.x, results[, select] - results$SE, plot.x, results[, select] + results$SE, code = 3, length = 0.02, angle = 90)
 }
 
 
@@ -66,7 +109,7 @@ plot.IBCF <- function(x, select = 'Pearson', ...){
 #' @description Barplot of the results from IBCFY object
 #'
 #' @param height \code{IBCFY object} IBCFY object, result of use the IBCF.Years() function
-#' @param select \code{character} By default ('Pearson'), plot the Pearson Correlations of the IBCF Object, else ('MSEP'), plot the MSEP of the IBCF Object.
+#' @param select \code{character} By default ('Pearson'), plot the Pearson Correlations of the IBCF Object, else ('MAAPE'), plot the MAAPE of the IBCF Object.
 #' @param ... Further arguments passed to or from other methods.
 #'
 #' @importFrom graphics barplot
@@ -75,15 +118,66 @@ barplot.IBCFY <- function(height, select = 'Pearson', ...){
   ### Check that object is compatible
   if (!inherits(height, "IBCFY")) stop("This function only works for objects of class 'IBCF'")
 
-  results <- height$predictions_Summary
+  results <- summary(height)
   vector <- as.numeric(paste(results[, select]))
   names(vector) <- results[, 1]
   vector <- vector[order(vector)]
 
   if (select == 'Pearson')
     select <- 'Pearson Correlation'
+  else
+    select <- 'MAAPE'
 
   barplot(vector, ylab = select, ...)
 }
 
+#' Print IBCF information object
+#'
+#' @param x object a
+#' @param ...  more objects
+#'
+#' @return test
+#' @importFrom utils head
+#' @export
+#'
+print.IBCF <- function(x, ...){
+  cat('Item Based Collaborative Filtering Model: \n',
+      'Fitted with ', x$NPartitions, ' random partitions\n',
+      'Runtime: ', x$executionTime ,' seconds \n\n',
+      'Some predicted values: \n')
 
+  print.default(format(head(x$predictions_Summary$Predicted, 20), digits = 3), print.gap = 2L, quote = FALSE)
+
+  cat('\nPredictive capacity of the model: \n')
+
+  print.data.frame(summary(x, 'compact', digits = 3), print.gap = 2L, quote = FALSE)
+
+  cat('\n Use str() function to found more datailed information.')
+  invisible(x)
+}
+
+#' Print IBCFY information object
+#'
+#' @param x object a
+#' @param ...  more objects
+#'
+#' @return test
+#' @importFrom utils head
+#' @export
+#'
+print.IBCFY <- function(x, ...){
+  cat('Item Based Collaborative Filtering Model: \n',
+      'Evaluated Environment/Year (s): ', x$Years.testing, '\n',
+      'Evaluated Trait (s): ', x$Traits.testing, '\n',
+      'Runtime: ', x$executionTime ,' seconds \n\n',
+      'Some predicted values: \n')
+
+  print.default(format(head(x$predictions_Summary$Predicted, 20), digits = 3), print.gap = 2L, quote = FALSE)
+
+  cat('\nPredictive capacity of the model: \n')
+
+  print.data.frame(summary(x, 'compact', digits = 3), print.gap = 2L, quote = FALSE)
+
+  cat('\n Use str() function to found more datailed information.')
+  invisible(x)
+}
